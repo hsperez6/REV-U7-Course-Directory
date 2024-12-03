7a75198f9ed3ab9d290ec7b8da666193baf6f80d
hsperez6 <118867950+hsperez6@users.noreply.github.com>
2024-12-03 14:57:50 -0800

    Initial Submit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom"; 
+
+// App Components
+import Home from "./components/Home";
+import About from "./components/About";
+import Teachers from "./components/Teachers";
+import Courses from "./components/Courses";
+import Header from "./components/Header";
+
+
+function App() {
+  return (
+    <div className="container">
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="teachers" element={<Teachers />} />
+        <Route path="courses" element={<Courses />} /> 
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
