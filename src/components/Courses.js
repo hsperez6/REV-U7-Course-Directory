1a400ee3682a3fcd7f6a5f59da9d6ba1d348c55d
hsperez6 <118867950+hsperez6@users.noreply.github.com>
2024-12-09 13:28:21 -0800

    React Router v6

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { Route, Routes, NavLink } from "react-router-dom"; 
+
+import HTML from "./courses/HTML"
+import CSS from "./courses/CSS"
+import JavaScript from "./courses//JavaScript"
 
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><a href='#'>HTML</a></li>
-        <li><a href='#'>CSS</a></li>
-        <li><a href='#'>JavaScript</a></li>
+        <li><NavLink to='html'>HTML</NavLink></li>
+        <li><NavLink to='css'>CSS</NavLink></li>
+        <li><NavLink to='javascript'>JavaScript</NavLink></li>
       </ul>
     </div>
+    <Routes>
+      <Route path='html' element={<HTML />} />
+      <Route path='css' element={<CSS />} />
+      <Route path='javascript' element={<JavaScript />} />
+    </Routes>
   </div>
 );
 
